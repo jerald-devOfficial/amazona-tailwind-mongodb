1f7bc33c076bbd0997b6fdd692336473f2f19f60
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2022-11-17 14:09:21 +0800

    Video-19-Load-Products-MongoDB

--- components/ProductItem.jsx
+++ components/ProductItem.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
-function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className='card'>
       <Link href={`/product/${product.slug}`}>
@@ -16,12 +16,14 @@ function ProductItem({ product }) {
         </Link>
         <p className='mb-2'>{product.brand}</p>
         <p>${product.price}</p>
-        <button className='primary-button' type='button'>
+        <button
+          className='primary-button'
+          type='button'
+          onClick={() => addToCartHandler(product)}
+        >
           Add to cart
         </button>
       </div>
     </div>
   );
 }
-
-export default ProductItem;
